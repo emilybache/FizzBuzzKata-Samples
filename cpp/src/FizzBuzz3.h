666d5cba8332fdd00946858f896930caf2ec8812
emily <emily@bacheconsulting.com>
2020-09-04 16:40:09 +0200

    third test

---
+++ cpp/src/FizzBuzz3.h
@@ -0,0 +1,15 @@
+#ifndef FIZZBUZZ_FIZZBUZZ3_H
+#define FIZZBUZZ_FIZZBUZZ3_H
+
+
+#include <vector>
+
+class FizzBuzz3 {
+public:
+    void fizzbuzz();
+    static std::vector<std::string> calculateFizzBuzz();
+    static std::string nToFizzBuzz(int n);
+};
+
+
+#endif //FIZZBUZZ_FIZZBUZZ3_H
