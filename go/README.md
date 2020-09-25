183ea7579f29f97efe5c598c0f34fc8955eb7cdf
emily <emily@bacheconsulting.com>
2020-09-25 10:26:51 +0200

    first sample and test working

--- go/README.md
+++ go/README.md
@@ -4,13 +4,13 @@
 
 Assuming you have a proper [workspace](http://golang.org/doc/code.html#Workspaces) set up, run
 ```
-go get github.com/[user]/FizzBizzKata-Samples/go/fizzbuzz
+go get github.com/[user]/FizzBuzzKata-Samples/go/fizzbuzz
 ```
 on the command line, with ```[user]``` replaced with a valid username.
 
 ## Running Tests
 
-On the command line, enter the ```.../FizzBizzKata-Samples/go``` directory and run
+On the command line, enter the ```.../FizzBuzzKata-Samples/go``` directory and run
 ```
 go test ./...
 ```
--- go/fizzbuzz/fizzbuzz_test.go
+++ go/fizzbuzz/fizzbuzz_test.go
@@ -1,18 +1,42 @@
-package marsrover
+package fizzbuzz
 
 import (
-	"testing"
 	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"os"
+	"testing"
 )
 
-func TestReceiveSingleCommandShouldMove(t *testing.T) {
-	plateau := Plateau{maxX: 5, maxY: 5}
-	startingPosition := Coordinates{1,9}
-	marsRover := MarsRover{plateau: plateau, heading: N, position: startingPosition}
+func expectedOutput() string {
+	return "1\n2\nFizz\n4\nBuzz\nFizz\n7\n8\nFizz\nBuzz\n11\nFizz\n13\n14\nFizzBuzz\n16\n17\nFizz\n19\nBuzz\nFizz\n22\n23\nFizz\nBuzz\n26\nFizz\n28\n29\nFizzBuzz\n31\n32\nFizz\n34\nBuzz\nFizz\n37\n38\nFizz\nBuzz\n41\nFizz\n43\n44\nFizzBuzz\n46\n47\nFizz\n49\nBuzz\nFizz\n52\n53\nFizz\nBuzz\n56\nFizz\n58\n59\nFizzBuzz\n61\n62\nFizz\n64\nBuzz\nFizz\n67\n68\nFizz\nBuzz\n71\nFizz\n73\n74\nFizzBuzz\n76\n77\nFizz\n79\nBuzz\nFizz\n82\n83\nFizz\nBuzz\n86\nFizz\n88\n89\nFizzBuzz\n91\n92\nFizz\n94\nBuzz\nFizz\n97\n98\nFizz\nBuzz\n"
+}
 
-	commands := []Command{B}
-	marsRover.acceptCommands(commands)
+func TestSample1(t *testing.T) {
 
-	expectedPosition := Coordinates{1,8}
-	assert.Equal(t, expectedPosition, marsRover.coordinates())
+	output := captureOutput(t, func() {
+		fizzbuzz1()
+	})
+	assert.Equal(t, expectedOutput(), output)
+}
+
+// code copied from https://groups.google.com/g/golang-nuts/c/hVUtoeyNL7Y
+func dieOn(err error, t *testing.T) {
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func captureOutput(t *testing.T, runnable func()) string {
+	realStdout := os.Stdout
+	defer func() { os.Stdout = realStdout }()
+	r, fakeStdout, err := os.Pipe()
+	dieOn(err, t)
+	os.Stdout = fakeStdout
+	runnable()
+	// need to close here, otherwise ReadAll never gets "EOF".
+	dieOn(fakeStdout.Close(), t)
+	newOutBytes, err := ioutil.ReadAll(r)
+	dieOn(err, t)
+	dieOn(r.Close(), t)
+	return string(newOutBytes)
 }
